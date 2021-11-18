835705f16a2c4d7dac4ceecdc6fd4da48c29ff34
Leandro Lopes <leandropesqueira7@gmail.com>
2021-11-18 18:05:33 -0400

    Project Create

---
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -0,0 +1,15 @@
+package com.eventoapp.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Evento {
+
+	private String nome;
+	private String local;
+	private String data;
+	private String horario;	
+	
+}
