824a394c0d758b2d8357ed01f26fa129be5a1e41
Leandro Lopes <leandropesqueira7@gmail.com>
2021-11-18 21:52:14 -0400

    Criando e configurando um banco de dados PostgreSQL

--- src/main/java/com/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -1,12 +1,27 @@
 package com.eventoapp.models;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Getter
 @Setter
-public class Evento {
-
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long codigo;
+	
 	private String nome;
 	private String local;
 	private String data;
