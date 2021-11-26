9f411030fa9746af2f78d18f9a6ecd655fbeed32
Leandro Lopes <leandropesqueira7@gmail.com>
2021-11-26 09:28:10 -0400

    Alterando profile test e prod

--- src/main/java/com/eventoapp/DataConfigurationPostgreSQL.java
+++ src/main/java/com/eventoapp/DataConfigurationPostgreSQL.java
@@ -7,6 +7,9 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 @Profile("prod")
@@ -27,6 +30,17 @@ public class DataConfigurationPostgreSQL {
         
        
         return basicDataSource;
-    }	
+    }
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.POSTGRESQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
 
 }
